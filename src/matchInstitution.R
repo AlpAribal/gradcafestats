fcfeff0ca9efff74481673e907de2d8169d87da3
Alp Aribal <alparbal@gmail.com>
2018-05-05 17:15:30 +0300

    better institution matching

--- src/matchInstitution.R
+++ src/matchInstitution.R
@@ -4,49 +4,50 @@ require(stringr)
 test <- T
 start <- Sys.time()
 submissions <- fread(input = '..\\data\\submissions.csv',
-                  sep = 'é',
-                  header = T,
-                  select = c('submissionId', 'institution'),
-                  quote = '')
+                     sep = 'é',
+                     header = T,
+                     select = c('submissionId', 'institution'),
+                     quote = '')
 
-matches <- fread(input = '..\\data\\matching.csv',
+matches <- fread(input = '..\\data\\matchInstitutions.csv',
                  sep = 'é',
                  header = T,
                  quote = '')
 
 submissions[, institution := str_to_lower(institution)]
+uniqueInsts <- unique(setdiff(submissions$institution, matches$submissionInst))
 
 insts_all <- fread(input = '..\\data\\institutions.csv',
                    header = T,
                    quote = '"')
 if(test){
-  insts_all <- insts_all[(nrow(insts_all)-2):nrow(insts_all)]
-  #insts_all <- insts_all[81]
+  #insts_all <- insts_all[(nrow(insts_all)-2):nrow(insts_all)]
+  #insts_all <- insts_all[1:50]
 }
 
-res <- as.data.table(matrix(data = 0, nrow = nrow(submissions), ncol = nrow(insts_all)+2))
-res[, 1] <- submissions[, submissionId]
+res <- as.data.table(matrix(data = 0, nrow = length(uniqueInsts), ncol = nrow(insts_all)+2))
+res[, 1] <- uniqueInsts
 res[, 2] <- NA
-colnames(res) <- c('submissionId', 'instId', insts_all[, instId])
+colnames(res) <- c('institution', 'matched_instId', insts_all[, instId])
 
 i <- 1
 repeat{
   print(paste0('Inst #', i))
   # Institution name should match rgx_match
   rgx <- insts_all[i , rgx_match]
-  res_vec <- str_detect(string = submissions[, institution], pattern = rgx)
-  
+  res_vec <- str_detect(string = uniqueInsts, pattern = rgx)
+
   # But it shouldn't match rgx_no_match
   rgx <- insts_all[i, rgx_no_match]
   if(!is.na(rgx) && rgx != ''){
-    res_vec <- res_vec & !str_detect(string = submissions[, institution], pattern = rgx)
+    res_vec <- res_vec & !str_detect(string = uniqueInsts, pattern = rgx)
   }
-  
+
   # Save the match result
   res[,i+2] <- res_vec
-  
+
   # If match, write Id
-  res[res_vec == T, instId := insts_all[i, instId]]
+  res[res_vec == T, matched_instId := insts_all[i, instId]]
   i <- i + 1
   if(i > nrow(insts_all)){
     break;
@@ -61,7 +62,7 @@ if(test){
 } else{
   res <- res[rSum==1, 1:2]
   write.table(x = res,
-              file = '..\\data\\matching.csv',
+              file = '..\\data\\matchInstitutions.csv',
               append = T,
               sep = 'é',
               row.names = F,
