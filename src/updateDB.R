42d12432a65dee65083313dd33841808dac3783d
Alp Aribal <alparbal@gmail.com>
2018-08-18 18:32:19 +0300

    new updating method, update db

--- src/updateDB.R
+++ src/updateDB.R
@@ -17,7 +17,7 @@ pageNo <- 1
 repeat {
   # Construct page URL
   pageURL <- paste0('http://thegradcafe.com/survey/index.php?q=%28a*%7Cb*%7Cc*%7Cd*%7Ce*%7Cf*%7Cg*%7Ch*%7Ci*%7Cj*%7Ck*%7Cl*%7Cm*%7Cn*%7Co*%7Cp*%7Cq*%7Cr*%7Cs*%7Ct*%7Cu*%7Cv*%7Cw*%7Cx*%7Cy*%7Cz*%7C1*%7C2*%7C3*%7C4*%7C5*%7C6*%7C7*%7C8*%7C9*%7C0*%7C%28*%7C%29*%7C_*%29&t=a&o=&pp=250&p=',pageNo)
-
+  
   # Download the page
   timer <- Sys.time()
   curlHandle <- getCurlHandle()
@@ -38,30 +38,27 @@ repeat {
   # Extract submissions from source code
   timer <- Sys.time()
   submissions <- readHTMLTable(sourceCode, colClasses = 'character', stringsAsFactors = FALSE)[[1]][-1,]
+  submissions <- as.data.table(submissions)
   
   # Modify 'Date Added' field
-  submissions[,"Date Added"] <- as.character(as.Date(submissions[,"Date Added"], format='%d %b %Y'))
+  submissions[, `Date Added` := as.character(as.Date(`Date Added`, format = '%d %b %Y'))]
   
   # Seperate major, degree, and semester
   regExp <- '^\\s*(.*)\\s*,\\s*(.*)\\s*\\((.\\d\\d|\\?)\\)$'
-  prog <- str_match_all(submissions[, "Program (Season)"], regExp)
-  prog <- matrix(unlist(prog), ncol = 4, byrow = TRUE)[,-1]
+  prog <- as.data.table(str_match(submissions$`Program (Season)`, regExp)[,-1])
   colnames(prog) <- c("major", "degree", "sem")
-  prog[prog[, "sem"] == '?', "sem"] <- NA
+  prog[sem == "?", sem := NA]
   
   # Seperate notification info
   regExp <- '^(Accepted|Rejected|Wait listed|Interview|Other) via (Postal Service|Phone|E-mail|Website|Other) on (\\d\\d? .{3} \\d{4})(?: Undergrad GPA: (.*)GRE General \\(V\\/Q\\/W\\): (.*)\\/(.*)\\/(.*)GRE Subject: (n\\/a|\\d*).*)?$'
-  notif <- str_match_all(submissions[, "Decision & Date"], regExp)
-  notif <- t(sapply(notif, function(x) {length(x) <- 9
-                                    as.character(x)
-                                    }))[,-1]
+  notif <- as.data.table(str_match(submissions$`Decision & Date`, regExp)[,-1])
   colnames(notif) <- c("notif_result", "notif_method", "notif_date", "gpa", "gre_v", "gre_q", "gre_w", "gre_subject")
-  notif[,"notif_date"] <- as.character(as.Date(notif[,"notif_date"], format='%d %b %Y'))
-  notif[notif[, "gpa"] == 'n/a', "gpa"] <- NA
-  notif[notif[, "gre_v"] == 'n/a', "gre_v"] <- NA
-  notif[notif[, "gre_q"] == 'n/a', "gre_q"] <- NA
-  notif[notif[, "gre_w"] == 'n/a', "gre_w"] <- NA
-  notif[notif[, "gre_subject"] == 'n/a', "gre_subject"] <- NA
+  notif[, notif_date := as.character(as.Date(notif_date, format = '%d %b %Y'))]
+  notif[gpa == 'n/a', gpa := NA]
+  notif[gre_v == 'n/a', gre_v := NA]
+  notif[gre_q == 'n/a', gre_q := NA]
+  notif[gre_w == 'n/a', gre_w := NA]
+  notif[gre_subject == 'n/a', gre_subject := NA]
   
   # Get submissionIds
   regExp <- 'hideControlsBox\\(this\\);" on(?:m|M)ouse(?:o|O)ver="showControlsBox\\(this,(\\d+)\\);'
@@ -70,44 +67,48 @@ repeat {
   colnames(subId) <- c('submissionId')
   
   # Merge results
-  results <- cbind(subId, submissions[, 'Date Added'], submissions[, 'Institution'], prog, notif[, 1:3], submissions[, 'St1'], notif[, 4:8], submissions[, 'Notes'])
-  results <- apply(results, 2, function(x) {gsub('^\\s+', '',x)})
-  results <- apply(results, 2, function(x) {gsub('\\s+$', '',x)})
-  colnames(results) <- c('submissionId', 'submissionDate', 'institution', 'major', 'degree', 'sem', 'notif_result', 'notif_method', 'notif_date', 'studentType', 'gpa', 'gre_v', 'gre_q', 'gre_w', 'gre_subject', 'notes')
+  results <- cbind(subId, submissionDate = submissions$`Date Added`
+                   , institution = submissions$Institution, prog, notif[, .(notif_result, notif_method, notif_date)]
+                   , submissions$St1, notif[, .(gpa, gre_v, gre_q, gre_w, gre_subject)], notes = submissions$Notes)
   
   numResults <- nrow(results)
   print(paste0('Parse complete (took ', Sys.time()-timer, ' seconds: ', numResults, ' results'))
   
   # Filter results based on the largest submissionId in the database
-  results <- results[as.numeric(results[,'submissionId']) > maxDB, ]
+  results <- results[as.numeric(submissionId) > maxDB]
+  
+  # Additional filters
+  results <- results[!str_detect(string = institution, regex("chnlove|sexy|love-sites", ignore_case = T)), ]
   
   # If there are no new submissions, stop
-  if(!is.matrix(results) && !(is.vector(results) && length(results) > 0)) {
+  if(nrow(results) > 0) {
     print('There are not any new submissions!')
     break
   }
   print(paste0('There are ', nrow(results), ' new submissions in page ', pageNo))
-  
-  # Correct empty fields
-  results[results == ''] <- NA
-  
-  # Delimiter for .csv files is 'é', remove all 'é' from data
-  results <- str_replace_all(results, 'é', '?')
-  
-  # Delete unnecessary line breaks
-  results <- str_replace_all(results, '\r\n', ' ')
-  results <- str_replace_all(results, '\n', ' ')
-  results <- str_replace_all(results, '\r', ' ')
-  
-  # Re-create the structure
-  dim(results) <- c(length(results)/16, 16)
-  
-  # Some errors occur because of encoding.
-  # !!! Following three rows get rid of encoding errors. I do not know why. !!!
-  Encoding(results) <- "unknown"
-  results <- enc2utf8(results)
-  Encoding(results) <- "unknown"
 
+  results <- results[, (colnames(results)) := lapply(.SD, function(x){
+    # Trim fields
+    x <- str_trim(x, side = "both")
+    
+    # Delimiter for .csv files is 'é', remove all 'é' from data
+    x <- str_replace_all(x, 'é', '?')
+
+    # Delete unnecessary line breaks
+    x <- str_replace_all(x, '\r\n', ' ')
+    x <- str_replace_all(x, '\n', ' ')
+    x <- str_replace_all(x, '\r', ' ')
+
+    # Correct empty fields
+    x[x == ''] <- NA
+
+    # Set encoding
+    Encoding(x) <- "UTF-8"
+
+    return(x)
+  })
+  , .SDcols = colnames(results)]
+  
   # Insert the results into database
   timer <- Sys.time()
   write.table(x = results,
@@ -119,7 +120,7 @@ repeat {
               quote = F)
   print(paste0('Results were inserted into database (took ', Sys.time()-timer, ' seconds) -----'))
   
-  newMaxDB <- max(results[, 1], newMaxDB)
+  newMaxDB <- max(results[, submissionId], newMaxDB)
   
   pageNo <- pageNo + 1
   # Stop if you are at the last page OR if not all results fetched from the current page were fresh 
